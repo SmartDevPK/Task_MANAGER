9bba18426c1dc0fd043ab00ffc6626342eeb5195
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-07 04:25:18 +0100

    feat: Add task creation feature to the codebase

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto} from './create_data.dto'
+import { InjectRepository } from '@nestjs/typeorm';
+import {Repository} from "typeorm"
+import { Task } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(
+        @InjectRepository(Task)
+        private tasksRepository: Repository<Task>,
+    ) {}
+
+     async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        const { title, description,  status} = createTaskDto;
+
+        const task = this.tasksRepository.create({
+            title,
+            description,
+            status
+        });
+         await this.tasksRepository.save(task);
+         return task;
+
+    }
+}
